047ecb5805541968b790958fa67b33c4e6b2a57c
Roshan Kewat <roshankewat9090@gmail.com>
2026-02-24 13:26:04 +0530

    Fixed google login

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,21 @@
+'use client';
+import { useSearchParams } from "next/navigation";
+
 import NonProtectedRoute from "./Wrappers/NonProtectedRoute";
+import { useEffect } from "react";
+
+
 
 export default function Home() {
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    if(process.env.NEXT_PUBLIC_NODE_ENV !== 'production' && searchParams.get('access_token')){
+      document.cookie = `access_token=${searchParams.get('access_token')};domain=.renxframe.test;path=/`
+    }
+  },[])
+
+
   return (
     <NonProtectedRoute>
       <div></div>
