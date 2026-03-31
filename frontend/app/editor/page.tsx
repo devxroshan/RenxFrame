5b12f5737e97abc7fb633c0266e0fad29f6fa125
Roshan Kewat <roshankewat9090@gmail.com>
2026-03-31 21:24:42 +0530

    created UI of hierarchy.

--- frontend/app/editor/page.tsx
+++ frontend/app/editor/page.tsx
@@ -3,7 +3,18 @@ import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 
 // Icons
-import { Undo, Redo, Monitor, Tablet, Smartphone } from "lucide-react";
+import {
+  Undo,
+  Redo,
+  Monitor,
+  Tablet,
+  Smartphone,
+  ChevronDown,
+  Box,
+  Type,
+  Image as ImageIcon,
+  Square,
+} from "lucide-react";
 import Button, { ButtonVariant } from "../components/Button";
 
 // Components
@@ -11,6 +22,7 @@ import Button, { ButtonVariant } from "../components/Button";
 // Stores
 import { useAppStore } from "../stores/app.store";
 import React, { useEffect, useRef, useState } from "react";
+import Input, { InputVariant } from "../components/Input";
 
 enum ScreenSize {
   MOBILE = "375px",
@@ -24,12 +36,30 @@ interface EditorProps {
   setZoom: React.Dispatch<React.SetStateAction<number>>;
 }
 
+const ZOOM_LEVELS = [10, 25, 50, 75, 80, 100, 125, 150, 175, 200, 250, 300];
+const DEFAULT_ZOOM = 0.8;
+
+const ElementsIcon = {
+  box: <Box className="w-4 h-4" />,
+  type: <Type className="w-4 h-4" />,
+  image: <ImageIcon className="w-4 h-4" />,
+  square: <Square className="w-4 h-4" />,
+};
+
+const Elements = [
+  { name: "Container", icon: "box" },
+  { name: "Text", icon: "type" },
+  { name: "Image", icon: "image" },
+  { name: "Button", icon: "square" },
+];
+
 const Editor = () => {
   // States
   const [currentScreenSize, setCurrentScreenSize] = useState<ScreenSize>(
     ScreenSize.DESKTOP,
   );
-  const [zoom, setZoom] = useState<number>(0.8);
+  const [zoom, setZoom] = useState<number>(DEFAULT_ZOOM);
+  const [selectZoom, setSelectZoom] = useState<boolean>(false);
 
   const searchParams = useSearchParams();
 
@@ -51,7 +81,7 @@ const Editor = () => {
 
             <Divider />
 
-            <span className="font-medium text-sm">
+            <span className="font-medium text-sm truncate">
               {
                 appStore.getSiteById(searchParams.get("site_id") as string)
                   ?.name
@@ -93,24 +123,34 @@ const Editor = () => {
 
               <Divider strokeWidth="2px" />
 
-              <div className="flex items-center gap-2">
-                <select
-                  className="outline-none font-medium text-primary-text border-none px-2 py-1 text-sm"
-                  onChange={(e) => setZoom(Number(e.target.value) / 100)}
-                  defaultValue={80}
+              <div className="flex flex-col items-center gap-2">
+                <div
+                  className="flex items-center justify-between gap-1 cursor-pointer"
+                  onClick={() => setSelectZoom(!selectZoom)}
                 >
-                  <option value="10">10%</option>
-                  <option value="50">50%</option>
-                  <option value="60">60%</option>
-                  <option value="70">70%</option>
-                  <option value="80">80%</option>
-                  <option value="90">90%</option>
-                  <option value="100">100%</option>
-                  <option value="150">150%</option>
-                  <option value="200">200%</option>
-                  <option value="250">250%</option>
-                  <option value="300">300%</option>
-                </select>
+                  <span className="text-primary-text font-medium text-sm px-2 py-1">{`${Math.round(zoom * 100)}%`}</span>
+                  <ChevronDown
+                    className="text-primary-text w-4 h-4 cursor-pointer"
+                    strokeWidth={2.5}
+                  />
+                </div>
+
+                {selectZoom && (
+                  <div className="absolute top-12 bg-primary-bg border border-primary-border rounded-lg w-24 py-1 flex flex-col items-start justify-center z-20 px-1">
+                    {ZOOM_LEVELS.map((level) => (
+                      <span
+                        key={level}
+                        onClick={() => {
+                          setZoom(level / 100);
+                          setSelectZoom(false);
+                        }}
+                        className="bg-primary-bg text-primary-text font-medium text-sm px-2 py-1 hover:bg-primary-blue hover:text-white rounded-md w-full text-left cursor-pointer transition-all duration-300"
+                      >
+                        {level}%
+                      </span>
+                    ))}
+                  </div>
+                )}
               </div>
             </div>
           </div>
@@ -142,7 +182,7 @@ const Editor = () => {
           </div>
         </div>
 
-        <section className="w-full h-[93%] flex items-center justify-center">
+        <section className="w-full h-[93%] flex items-center justify-center overflow-hidden">
           <Hierarchy />
           <MainEditor
             currentScreenSize={currentScreenSize}
@@ -166,9 +206,70 @@ const Divider = ({ strokeWidth }: { strokeWidth?: string }) => {
 };
 
 const Hierarchy = () => {
+  const [currentResource, setCurrentResource] = useState<
+    "components" | "assets"
+  >("components");
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
   return (
     <>
-      <main className="w-[20%] h-full bg-primary-bg border-r border-primary-border"></main>
+      <main className="w-[20%] flex flex-col gap-4 px-2 py-2 h-full bg-primary-bg border-r border-primary-border items-center justify-start">
+        <div className="w-full h-fit flex gap-2 items-center justify-between">
+          <button
+            className={`text-sm font-medium px-3 py-1 rounded-md cursor-pointer w-full transition-all duration-300 ${currentResource === "components" ? "bg-white text-black" : "bg-tertiary-bg text-primary-text hover:bg-gray-100/10 hover:text-white"}`}
+            onClick={() => setCurrentResource("components")}
+          >
+            Components
+          </button>
+
+          <button
+            className={`text-sm font-medium px-3 py-1 rounded-md cursor-pointer w-full transition-all duration-300 ${currentResource === "assets" ? "bg-white text-black" : "bg-tertiary-bg text-primary-text hover:bg-gray-100/10 hover:text-white"}`}
+            onClick={() => setCurrentResource("assets")}
+          >
+            Assets
+          </button>
+        </div>
+
+        <Input
+          variant={InputVariant.PRIMARY}
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+
+        <div className="w-full h-full flex flex-col items-start justify-start gap-2">
+          <span className="text-sm text-primary-text font-semibold">
+            Basic Elements
+          </span>
+
+          <div className="w-full h-full flex flex-col items-center justify-start gap-2">
+            {Elements.filter((element) =>
+              element.name.toLowerCase().includes(searchTerm.toLowerCase()),
+            ).map((element) => (
+              <div
+                key={element.name}
+                className="w-full px-3 py-2 bg-tertiary-bg border border-primary-border rounded-xl flex items-center justify-start gap-2 cursor-pointer hover:bg-secondary-bg transition-all duration-300 hover:text-white text-primary-text"
+              >
+                {ElementsIcon[element.icon as keyof typeof ElementsIcon]}
+                <span className="text-sm font-medium transition-all duration-300 w-full text-left">
+                  {element.name}
+                </span>
+              </div>
+            ))}
+
+            {searchTerm &&
+              !Elements.some((e) =>
+                e.name.toLowerCase().includes(searchTerm.toLowerCase()),
+              ) && (
+                <div className="w-full h-full flex items-center justify-center">
+                  <span className="text-primary-text font-semibold">
+                    No elements found
+                  </span>
+                </div>
+              )}
+          </div>
+        </div>
+      </main>
     </>
   );
 };
