39897d40845fe09d966e1ebc01bd5a99bc451557
Roshan Kewat <roshankewat9090@gmail.com>
2026-03-31 23:12:31 +0530

    Fixed.

--- backend/src/modules/workspace/workspace.controller.ts
+++ backend/src/modules/workspace/workspace.controller.ts
@@ -3,8 +3,6 @@ import { WorkspaceService } from "./workspace.service";
 import { CreateWorkspaceDto } from "./dto/create-workspace.dto";
 import * as express from 'express'
 import { IsLoggedInGuard } from "src/common/guards/is-logged-in.guard";
-import { ValidateMongoId } from "src/common/decorators/ValidateMongoId.decorator";
-import { ValidateMongoIdGuard } from "src/common/guards/validate-mongo-id.guard";
 
 @Controller('workspace')
 @UseGuards(IsLoggedInGuard)
@@ -23,8 +21,6 @@ export class WorkspaceController {
         return this.workspaceService.getAllWorkspace(req.user?.id)
     }
 
-    @ValidateMongoId({param: ['workspace_id']})
-    @UseGuards(ValidateMongoIdGuard)
     @Get(':workspace_id')
     async getWorkspace(@Req() req:express.Request) {
         return this.workspaceService.getAllWorkspace(req.params?.workspace_id as string)
